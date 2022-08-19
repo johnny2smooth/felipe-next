a1e7af4847877531a1499f0c35efcfca3e892321
johnny2smooth <johnbp13@gmail.com>
2022-08-19 10:54:56 -0700

    photo tab logic introduced and working

--- components/navbar.js
+++ components/navbar.js
@@ -4,8 +4,6 @@ import { useRouter } from 'next/router';
 export default function Navbar() {
   const router = useRouter();
 
-  console.log(router);
-
   const links = [
     { route: '/about', text: 'About', id: 'about' },
     { route: '/practice', text: 'Practice', id: 'projects' },
@@ -16,7 +14,7 @@ export default function Navbar() {
   return (
     <div className="flex flex-wrap items-baseline px-6">
       <div className="flex grow flex-wrap items-baseline pt-6">
-        <h1 className="text-6xl font-bold hover:underline hover:text-[#4969ED]">
+        <h1 className="text-6xl font-bold hover:underline ">
           <Link href="/">
             <a>Felipe Matamala</a>
           </Link>
@@ -24,7 +22,7 @@ export default function Navbar() {
         <p>psychoanalyst</p>
       </div>
       <nav>
-        <ul className="flex flex-wrap [&>*]:pr-6 py-6 [&>*:hover]:underline text-lg">
+        <ul className="flex flex-wrap [&>*]:pr-6 py-6 [&>*:hover]:underline  text-lg">
           {links.map(({ route, text, id }) => (
             <li key={id}>
               <Link href={route}>
--- components/photoTabs.js
+++ components/photoTabs.js
@@ -1,26 +1,46 @@
+import { useState } from 'react';
 import Image from 'next/image';
 
 function PhotoTabs() {
+  const [tab, setTab] = useState(1);
+
   let data = [
-    { src: '/contact-sticker.png', alt: 'alt', topic: 'Press me' },
-    { src: '/contact-sticker.png', alt: 'alt', topic: 'Press me' },
-    { src: '/contact-sticker.png', alt: 'alt', topic: 'Press me' },
+    { src: '/contact-sticker.png', alt: 'alt', topic: '1', index: 1 },
+    { src: '/contact-sticker.png', alt: 'alt', topic: '2', index: 2 },
+    { src: '/contact-sticker.png', alt: 'alt', topic: '3', index: 3 },
   ];
+
+  const handleTabChange = (e) => {
+    setTab(e.target.value);
+  };
 
   return (
     <div className="flex flex-col items-center justify-center my-8">
-      <div className="my-8 grid gap-2 grid-cols-3 border-b border-gray-200">
-        {data.map((item) => (
-          <button
-            key={item.topic}
-            className="text-center flex flex-col items-center mb-4 border-b border-gray-200 hover:border-red-200"
+      <div className="my-8 grid gap-2 grid-cols-3 border-b bg-slate-100 rounded-2xl border-gray-200 p-2">
+        {data.map(({ src, alt, topic, index }) => (
+          <div
+            key={topic}
+            className={`index text-center flex flex-col  mb-1 border-b border-gray-200 hover:border-red-200`}
           >
-            <Image src={item.src} height={200} width={200} alt={item.alt} />
-            <p className="inline-block p-4 rounded-t-lg border-b-2 hover:border-red-200">
-              {item.topic}
-            </p>
-          </button>
+            <Image src={src} height={200} width={200} alt={alt} />
+            <button
+              className={`${
+                tab === index ? 'bg-slate-400 shadow-sm' : ''
+              } inline-block p-4 rounded-t-lg bg-slate-300 hover:bg-slate-400 active:bg-slate-400`}
+              onClick={handleTabChange}
+              value={index}
+            >
+              {topic}
+            </button>
+          </div>
         ))}
+        <p className={`col-span-3`}>
+          {tab === '3'
+            ? '3333333 something so special that it needs a description!'
+            : tab === '2'
+            ? '2222222 something so special that it needs a description!'
+            : '1111111 something so special that it needs a description!'}
+        </p>
       </div>
     </div>
   );
